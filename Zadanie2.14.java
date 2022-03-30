3fea5e9e6a450f74530b480dea8668b3d579ba09
DanielPszczola <102531727+DanielPszczola@users.noreply.github.com>
2022-03-30 16:16:39 +0200

    Add files via upload

---
+++ Zadanie2.14.java
@@ -0,0 +1,38 @@
+package Zadanie214;
+
+import java.util.Scanner;
+
+public class Zadanie214 {
+
+    public static boolean czyParzysta(int liczba){
+
+        return liczba % 2 == 0;
+
+    }
+
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int liczba;
+        System.out.println("Podaj dowolna liczbe:");
+
+        liczba = scanner.nextInt();
+
+        if ( czyParzysta(liczba) == true){
+
+            System.out.println(liczba + " jest parzysta");
+
+        }
+        else {
+
+            System.out.println(liczba + " jest nieparzysta");
+
+        }
+
+
+    }
+
+
+}
