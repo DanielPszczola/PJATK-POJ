9850a03ba1725e0d0b34ddad6c3a3fad4b483a66
DanielPszczola <102531727+DanielPszczola@users.noreply.github.com>
2022-05-31 20:05:03 +0200

    Lab5

--- Zadanie5_1/Arraylist.java
+++ Zadanie5_1/Arraylist.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Arraylist {
 
     public static void main(String args[]){
-
         ArrayList arrayList = new ArrayList();
 
 
@@ -25,5 +24,5 @@ public class Arraylist {
 
         }
     }
+    }
 
-}
